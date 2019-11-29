f2f7574dd9e0d281fd4760fdaa9db03c29294b8d
yoseplee <joseph38.local@gmail.com>
2019-11-29 15:06:18 +0900

    update navigation in App.vue

--- chart-js-demo/src/App.vue
+++ chart-js-demo/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/chartsjs">vue-chartjs</router-link> |
+      <router-link to="/charts">vue-charts</router-link> |
+      <router-link to="/chartkick">vue-chartkick</router-link>
     </div>
     <router-view/>
   </div>
