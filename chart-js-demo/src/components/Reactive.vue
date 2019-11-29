ca1a61fcbabac1c5880ceaa64be13366eb4fb2f0
yoseplee <joseph38.local@gmail.com>
2019-11-29 17:51:48 +0900

    add reactive.vue

---
+++ chart-js-demo/src/components/Reactive.vue
@@ -0,0 +1,41 @@
+<script>
+//Import Bar and mixins class from the vue-chartjs wrapper
+import { Bar, mixins } from 'vue-chartjs'
+
+//Getting the reactiveProp mixin from the mixins module.
+const { reactiveProp } = mixins
+export default Bar.extend({
+    mixins: [reactiveProp],
+    data() {
+        return {
+            //Chart.js options that control the appearance of the chart
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        gridLines: {
+                            display: true
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: true
+                        }
+                    }]
+                },
+                legend: {
+                    display: true
+                },
+                responsive: true,
+                maintainAsepectRatio: false
+            }
+        }
+    },
+    mounted() {
+        //this.chartData is created in the mixin and contains all the data needed to build chart.
+        this.renderChart(this.chartData, this.options)
+    }
+})
+</script>
