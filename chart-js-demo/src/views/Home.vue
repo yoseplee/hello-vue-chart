fcbd53c2325133d5e4d0afc1c5d301d8f35ea744
yoseplee <joseph38.local@gmail.com>
2019-11-29 15:09:49 +0900

    update HomeComponent to introduce what to do

--- chart-js-demo/src/views/Home.vue
+++ chart-js-demo/src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1>Creating Beautiful Charts Using Vue.js Wrappers For Chart.js</h1>
+        <h2>
+          Read the article on SitePoint:
+          <a href="#">Creating Beautiful Charts Using Vue.js Wrappers for Chart.js</a>
+        </h2>
+        <h3>
+          Download the repo from github:
+          <a href="#">https://github.com/sitepoint-editors/vue-charts</a>
+        </h3>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,8 +22,5 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
