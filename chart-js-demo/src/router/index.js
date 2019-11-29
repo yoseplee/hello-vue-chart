65cf2bcfd53e485b3eb889c936f08d81b5574143
yoseplee <joseph38.local@gmail.com>
2019-11-29 15:04:23 +0900

    update rounter for charts

--- chart-js-demo/src/router/index.js
+++ chart-js-demo/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import VueChartJS from '../views/VueChartJS.vue'
+import VueChartKick from '../views/VueChartKick.vue'
+import VueCharts from '../views/VueCharts.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/chartjs',
+    name: 'VueChartJS',
+    component: VueChartJS
+  },
+  {
+    path: '/chartkick',
+    name: 'VueChartKick',
+    component: VueChartKick
+  },
+  {
+    path: '/charts',
+    name: 'VueCharts',
+    component: VueCharts
+  },
 ]
 
 const router = new VueRouter({
